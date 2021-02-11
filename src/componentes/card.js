a2ef60ffcd968030cc996aed52cb4b47354366e4
Fabianvminotti <fabianvminotti@gmail.com>
2021-02-11 04:05:23 -0300

    ordenar archivos

---
+++ src/componentes/card.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+class Card extends React.Component{
+	render(){
+		return(
+			<div>
+				<h1> Pelicula1 </h1>
+				<img/>
+				<p> Descripcon de la pelicula</p>
+
+			</div>
+
+
+			)
+	}
+}
+
+
+export default Card
