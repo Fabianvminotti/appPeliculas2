14e990e90c8e9d66383f04c8383e83e5c887f096
Fabianvminotti <fabianvminotti@gmail.com>
2021-02-21 05:13:27 -0300

    script para agregar peli

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -25,6 +25,23 @@ class Menu extends React.Component{
 			this.setState({panelAgregar:false})
 		}
 
+		agregar = (e)=>{
+			e.preventDefault();
+			this.setState({panelAgregar:false})
+
+			const titulo = this.state.titulo;
+			const desc =  this.state.desc;
+			const calif = this.state.calif;
+			const Img = this.state.img;
+
+			this.props.agregarAux({titulo:titulo,
+								desc:desc,
+								calif:calif,
+								Img:Img})
+
+
+		}
+
 		render(){
 			return(
 					<div>
@@ -41,19 +58,21 @@ class Menu extends React.Component{
 								<button onClick={this.add} className="agregar">Nuevo libro</button>
 							</div>
 						</div>
-
 						{  /* esta es una forma particular del if */
 							(this.state.panelAgregar)?
-							<PanelAgregar cancelar={this.cancelar}/> //(mandar a llamar al prop)aca tengo que definir "cancelar" que se trae desde el elemento PanelAgregar
+							<PanelAgregar cancelar={this.cancelar} agregar={this.agregar}/> //(mandar a llamar al prop)aca tengo que definir "cancelar" que se trae desde el elemento PanelAgregar
 							:
 							" " 
-											
 						}
+						
 					</div>
 
 				)
 			}
 }
+
+
+
 
 export default Menu;
 
--- src/componentes/PanelAgregar.js
+++ src/componentes/PanelAgregar.js
@@ -1,12 +1,96 @@
 import React from 'react'
 
 
-function PanelAgregar (props){
-	return(
-			<div>laevublaublurbvlkurvb <button onClick={props.cancelar}>Cancelar</button></div>
+class PanelAgregar extends React.Component{
+	
+	constructor(props){
+        super(props);
+
+		this.state={
+			titulo:"",
+			desc:"",
+			calif:1,
+			Img:""
+		};
+		
+        
+    }
+
+	setCalificacion = (e) =>{
+		this.setState({calif: e.target.setCalificacion})
+	}
+
+	setTitulo = (e) =>{
+		this.setState({titulo: e.target.setTitulo})
+	}
+
+	setImg = (e) =>{
+		this.setState({img: e.target.setImg})
+	}
+
+	setDescripcion = (e) =>{
+		this.setState({desc: e.target.setDescripcion})
+	}
+
+
+	// valor = () => {
+	// 	document.getElementById('range')
+	// }
+
+
+	render(){
+			return(
+					<div className="addPelicula-contenedor">
+					<form className="addPelicula">
+						<p>Titulo</p>
+						<input onChange={this.setTitulo} ></input>
+						<p>Descripcion</p>
+						<input onChange={this.setDescripcion} ></input>
+						<p>Calificacion:{this.setCalificacion} </p>
+						<input type="range"  min="1" max="5" id="range" onChange={this.setCalificacion}></input> 
+						
+						<p>Imagen</p>
+						<input  placeholder="Insetar la URL" onChange={this.setImg} ></input>
+					</form>	
+						
+						
+						<button onClick={this.props.agregar}>Agregar</button>
+						<button onClick={this.props.cancelar}>Cancelar</button>
+					</div>
+					
+
+				)
+			}
+}
+
+
+// function PanelAgregar (props){
+// 	return(
+// 			<div>
+// 			<form className="addPelicula">
+// 				<p>Titulo</p>
+// 				<input type="text" ></input>
+// 				<p>Descripcion</p>
+// 				<input type="text" ></input>
+// 				<p>Calificacion: </p>
+// 				<input type="range"  min="1" max="5" id="range"></input> 
+// 				{/* <input value="1" onChange="document.getElementById('range').value=this.value" > </input> */}
+// 				<p>Imagen</p>
+// 				<input type="text"  placeholder="Insetar la URL" ></input>
+// 			</form>	
+				
+				
+// 				<button onClick={props.agregar}>Agregar</button>
+// 				 <button onClick={props.cancelar}>Cancelar</button>
+// 			</div>
 			
 
-		)
-}
+// 		)
+// }
+
+
+
+
+
 
 export default PanelAgregar;
